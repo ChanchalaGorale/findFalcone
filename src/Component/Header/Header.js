edaeb4d76da1c7218785fba4cfd9f657ac15c2ba
Chanchala G <cgorale111@gmail.com>
2021-02-10 21:52:01 +0530

    update

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -2,21 +2,26 @@ import React, { Component } from "react";
 import "./Header.css";
 import { title, geektrusthome } from "../../assets/data";
 import Logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
 class Header extends Component {
   state = {};
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">
-          <img
-            src={Logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top nav-logo"
-            alt="King Shan"
-          />
-          &nbsp; <strong>{title}</strong>
-        </a>
+        <Link to="/">
+          <button className="navbar-brand">
+            <img
+              src={Logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top nav-logo"
+              alt="King Shan"
+            />
+            &nbsp; <strong>{title}</strong>
+          </button>
+        </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -31,19 +36,20 @@ class Header extends Component {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="/">
-                Home
-              </a>
+              <Link to="/">
+                <button className="nav-link">Home</button>
+              </Link>
             </li>
             <li className="nav-item">
-              <a
-                className="nav-link"
-                href="/findFalcone"
-                data-toggle="tooltip"
-                title="Reset the find operation"
-              >
-                Start / Reset
-              </a>
+              <Link to="/findFalcone">
+                <button
+                  className="nav-link"
+                  data-toggle="tooltip"
+                  title="Reset the find operation"
+                >
+                  Start / Reset
+                </button>
+              </Link>
             </li>
             <li className="nav-item">
               <a

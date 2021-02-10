fe136ee49a7e20fedbac3ca0cd382d987b68e825
Chanchala G <cgorale111@gmail.com>
2021-02-10 22:22:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import FinalDestination from "./Component/Game/FinalDestination/Finaldestination
 import Success from "./Component/Game/Success/Success";
 import Home from "./Component/Game/Home/Home";
 import Knowmore from "./Component/Game/Home/Knowmore";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <Router>
+      <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/knowmore" exact component={Knowmore} />
           <Route path="/findFalcone" exact component={FinalDestination} />
           <Route path="/success" exact component={Success} />
         </Switch>
-      </Router>
+      </BrowserRouter>
       <Footer />
-    </>
+    </div>
   );
 }
 
